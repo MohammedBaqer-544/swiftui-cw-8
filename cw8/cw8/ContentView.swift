0880474640c2c7a3701cdcc78099a1eb9309251d
MohammedBaqer-544 <almousawi2002@gmail.com>
2020-12-29 21:01:40 +0300

    done

---
+++ cw8/cw8/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  cw8
+//
+//  Created by Mohammed on 12/29/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var counter = [0, 0, 0]
+    
+    var body: some View {
+        VStack {
+            thekerBtn(counter: $counter[0], lable: "أستغفر الله العظيم")
+            thekerBtn(counter: $counter[1], lable: "سبحان الله وبحمده")
+            thekerBtn(counter: $counter[2], lable: "الحمدلله")
+
+        
+    }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct thekerBtn: View {
+    
+    @Binding var counter: Int
+    let lable: String
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            
+            Text(lable)
+                .font(.title)
+            
+            Button(action: {
+                counter += 1
+            }, label: {
+                Text("\(counter)")
+                    .frame(width: 75.0, height: 75.0)
+                    .foregroundColor(.white)
+                    .background(Color.green)
+                    .clipShape(Circle())
+                    .padding()
+                    
+            })
+        }
+    }
+}
---
+++ cw8/cw8/cw8App.swift
@@ -0,0 +1,17 @@
+//
+//  cw8App.swift
+//  cw8
+//
+//  Created by Mohammed on 12/29/20.
+//
+
+import SwiftUI
+
+@main
+struct cw8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
